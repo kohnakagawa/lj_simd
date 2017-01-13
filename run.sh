6151e7a8516b8daacb17c951bfd770819bca88df
kohnakagawa <tsunekou1019@gmail.com>
2017-01-13 15:53:23 +0900

    for 1x8 8x1 kernels

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-execs=("./comp_1x4.out" "./comp_4x1.out" "./comp_ref.out")
-files=("1x4.txt" "4x1.txt" "ref.txt")
+make clean
+make
+
+execs=("./comp_1x4.out" "./comp_4x1.out" "./comp_ref_4.out" "comp_1x8_v2.out" "comp_8x1_v2.out" "./comp_ref_8.out")
+files=("1x4.txt" "4x1.txt" "ref_4.txt" "1x8.txt" "8x1.txt" "ref_8.txt")
 Ni="100003"
 seed=$RANDOM
 
@@ -17,6 +20,8 @@ do
     done
 done
 
-echo "# num_iloop num_jloop 1x4 4x1 ref" > result_$seed.txt
-paste 1x4.txt 4x1.txt ref.txt | awk '{print $1, $2, $3, $7, $11, $4}' >> result_$seed.txt
+echo "# num_iloop num_jloop 1x4 4x1 ref" > result4_$seed.txt
+echo "# num_iloop num_jloop 1x8 8x1 ref" > result8_$seed.txt
+paste 1x4.txt 4x1.txt ref_4.txt | awk '{print $1, $2, $3, $7, $11, $4}' >> result4_$seed.txt
+paste 1x8.txt 8x1.txt ref_8.txt | awk '{print $1, $2, $3, $7, $11, $4}' >> result8_$seed.txt
 rm ${files[@]}
